7da4c8216bd6fae2b92b51716c9d9721edb06109
ThatGuyCalledJesse <72275409+ThatGuyCalledJesse@users.noreply.github.com>
2022-07-04 20:36:00 +0200

    Add files via upload

---
+++ CaesarCipher/main.py
@@ -0,0 +1,22 @@
+from algorithm.algorithms import *
+from utils.encrypt import encrypt
+
+# TODO: Write the program that actually encrypts and decrypts a string (Done)
+# TODO: Add a menu that lets the user choose between encrypting and decrypting (In Progress)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e']
+
+# characterIndex1 = 0
+# for letter in alphabet:
+#     characterIndex2 = characterIndex1 + 5
+#     print(f"\"{alphabet[characterIndex1]}\": \"{alphabet[characterIndex2]}\",")
+#     characterIndex1 += 1
+
+# Driver code
+if __name__ == "__main__":
+    encryptString = input("What string do you want to encrypt? Write down the algorithm, or else you can't decrypt it!\n")
+    encrypted = encrypt(encryptString, chooseAlgorithm())
+    print(encrypted)
+    with open("output.txt", "a") as file:
+        file.write(f"\nEncrypted string: {encrypted}")
+    print("For your conveniences, the output is also written to a file called output.txt")
