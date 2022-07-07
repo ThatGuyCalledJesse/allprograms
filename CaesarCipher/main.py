399fc0a95910740f9199ae0271b935cf29f52cec
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-07-07 16:22:48 +0200

    Removed the algorithm-making part of the code

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -2,15 +2,9 @@ from allprograms.CaesarCipher.algorithm.algorithms import *
 from utils.encrypt import encrypt
 
 # TODO: Write the program that actually encrypts and decrypts a string (Done)
-# TODO: Add a menu that lets the user choose between encrypting and decrypting (Should probably start working on this)
+# TODO: Add a menu that lets the user choose between encrypting and decrypting (In Progress)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-characterIndex1 = 0
-for letter in alphabet:
-    characterIndex2 = characterIndex1 + 25
-    print(f"\"{alphabet[characterIndex1]}\": \"{alphabet[characterIndex2]}\",")
-    characterIndex1 += 1
 
 # Driver code
 if __name__ == "__main__":
