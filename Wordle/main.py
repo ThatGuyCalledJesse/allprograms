9b8f4d84a0ec996e34f6d41423e51fa3f94ca7c7
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-07-10 17:08:43 +0200

    Updated main.py

--- Wordle/main.py
+++ Wordle/main.py
@@ -2,4 +2,9 @@ from words import words
 import random
 
 word = random.choice(words)
-print(word)
+inputWord = input("Enter a 5-letter word: ")
+
+# Checks if the word is 5 letters long
+if len(inputWord) != 5:
+    raise Exception("It has to be 5 letters")
+
