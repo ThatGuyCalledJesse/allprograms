50d89f43cca3276e1cd8cd920c33c6af4bb98dbf
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-07-16 21:43:43 +0200

    Added the code

--- DataGenerator/create_data.py
+++ DataGenerator/create_data.py
@@ -2,7 +2,22 @@ import random
 import os
 
 def create_data(amountOfPlayers: int):
-    results = []
-    for i in range(5):
-        results.append(random.choice(("Heads", "Tails")))
+    # List that contains all results
+    all_results = []
 
+    # Clears the already existing file
+    with open("data.txt", "w") as file:
+        file.write("")
+
+    # A loop that iterates through all the players
+    for i in range(amountOfPlayers):
+        playerIndex = i + 1
+        result= []
+        for i in range(5):
+            result.append(random.choice(("Heads", "Tails")))
+        with open("data.txt", 'a') as file:
+            file.write(f'Player {playerIndex}: {result}\n')
+        all_results.append(result)
+    return all_results
+
+create_data(10)
