c34ef8602bd3be7d4882b68124a0171fbf22d469
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-07-17 12:41:37 +0200

    Added the right code

--- CheaterCatcher/validate.py
+++ CheaterCatcher/validate.py
@@ -4,5 +4,5 @@ def validate(data: list):
     playerIndex = 0
     for i in data:
         playerIndex += 1
-        if data[0] == data[1] and data[1] == data[2] and data[2] == data[3] and data[3] == data[4] and data[4] == data[5] and data[5] == "heads".lower():
+        if i[0] == i[1] and i[1] == i[2] and i[2] == i[3] and i[3] == i[4] and i[4] == "Heads":
             print(f"Player {playerIndex} is probably a cheater and returned positive from the test!")
