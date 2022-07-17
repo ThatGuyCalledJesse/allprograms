c7ad7a4fcdd754b5ec3b3078da770909d9e8df1f
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-07-17 12:37:09 +0200

    Added the code

--- CheaterCatcher/main.py
+++ CheaterCatcher/main.py
@@ -1,7 +1,8 @@
 from create_data import create_data
+from validate import validate
 
 # This program is used to catch cheaters based on math, not 100% accurate
 
 # Create player data for 1000 players
 data = create_data(1000)
-
+validate(data)
--- CheaterCatcher/validate.py
+++ CheaterCatcher/validate.py
@@ -1,4 +1,8 @@
 # A function that checks if the player
 
 def validate(data: list):
-    pass
+    playerIndex = 0
+    for i in data:
+        playerIndex += 1
+        if data[0] == data[1] and data[1] == data[2] and data[2] == data[3] and data[3] == data[4] and data[4] == data[5] and data[5] == "heads".lower():
+            print(f"Player {playerIndex} is probably a cheater and returned positive from the test!")
