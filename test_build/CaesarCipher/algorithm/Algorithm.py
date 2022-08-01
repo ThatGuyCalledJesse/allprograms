bf7a42fb063a8377b6579f0704b63af42e0add47
ThatGuyCalledJesse <jesseswart08@gmail.com>
2022-08-01 19:09:43 +0200

    Reorganized code

--- test_build/CaesarCipher/algorithm/Algorithm.py
+++
@@ -1,7 +0,0 @@
-# Don't change anything, this is to make the coding easier
-
-class Algorithm:
-    def __init__(self, algorithm: int, ciphering: dict, name: str):
-        self.algorithm = algorithm
-        self.ciphering = ciphering
-        self.name = name
